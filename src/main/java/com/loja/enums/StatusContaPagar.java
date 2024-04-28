9fd2bc411833b4d7a39d00a173c4f5d1f20e8104
Geovane Rodrigues <geovane.net@live.com>
2024-04-28 16:32:40 -0300

    criacao de modelos de conta
    modelos de conta a pagar e conta a receber e forma pagamento

---
+++ src/main/java/com/loja/enums/StatusContaPagar.java
@@ -0,0 +1,30 @@
+package com.loja.enums;
+
+public enum StatusContaPagar {
+	
+	COBRANCA("pagar"),
+	VENCIDA("Vencida"),
+	ABERTA("Aberta"),
+	QUITADA("Quitada"),
+	ALUGUEL("Aluguel"),
+	FUNCIONARIO("Funcionario"),
+	NEGOCIADA("Renegociada");
+	
+	private String descricao;
+	
+ 
+	private StatusContaPagar(String descricao) {
+		this.descricao =  descricao;
+	}
+	
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return this.descricao;
+	}
+	
+}
