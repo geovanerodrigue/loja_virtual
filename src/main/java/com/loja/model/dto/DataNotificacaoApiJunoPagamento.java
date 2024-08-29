ceac7f7a9a8fb440666fb2c07af3fe2b6605e7a8
Geovane Rodrigues <geovane.net@live.com>
2024-08-28 21:09:18 -0300

    att-28-08-2024
    criação de webhook para receber notificação de pagamento

---
+++ src/main/java/com/loja/model/dto/DataNotificacaoApiJunoPagamento.java
@@ -0,0 +1,50 @@
+package com.loja.model.dto;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataNotificacaoApiJunoPagamento implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+
+	private String eventId;
+	private String eventType;
+	private String timestamp;
+
+	private List<AttibutesNotificaoPagaApiJuno> data = new ArrayList<AttibutesNotificaoPagaApiJuno>();
+
+	public String getEventId() {
+		return eventId;
+	}
+
+	public void setEventId(String eventId) {
+		this.eventId = eventId;
+	}
+
+	public String getEventType() {
+		return eventType;
+	}
+
+	public void setEventType(String eventType) {
+		this.eventType = eventType;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public List<AttibutesNotificaoPagaApiJuno> getData() {
+		return data;
+	}
+
+	public void setData(List<AttibutesNotificaoPagaApiJuno> data) {
+		this.data = data;
+	}
+
+}
--- src/main/java/com/loja/security/WebConfigSecurity.java
+++ src/main/java/com/loja/security/WebConfigSecurity.java
@@ -32,6 +32,8 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements H
 		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 		.disable().authorizeRequests().antMatchers("/").permitAll()
 		.antMatchers("/index").permitAll()
+		.antMatchers(HttpMethod.POST, "/requisicaojunoboleto/**", "/notificacaoapiv2").permitAll()
+		.antMatchers(HttpMethod.GET, "/requisicaojunoboleto/**", "/notificacaoapiv2").permitAll()
 		.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 
 		//redireciona ou da  um retorno para index quando desloga
@@ -57,9 +59,9 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements H
 	//ignora algumas url livre de autenticacao
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-      //web.ignoring()
-      //.antMatchers(HttpMethod.GET, "/salvarAcesso", "/deleteAcesso")
-     // .antMatchers(HttpMethod.POST, "/salvarAcesso", "/deleteAcesso");
+      web.ignoring()
+      .antMatchers(HttpMethod.GET, "/requisicaojunoboleto/**")
+      .antMatchers(HttpMethod.POST, "/requisicaojunoboleto/**");
 	}
 
 }
